c87b6d0fb26d3fedbeaa95af28a5ce24a64f45b1
TanyaKost <taaapv@gmail.com>
2022-12-20 14:48:56 +0400

    Change State Object to Observed Object Detail view model

--- HeroSwiftUIMVVM/Detail/DetailView.swift
+++ HeroSwiftUIMVVM/Detail/DetailView.swift
@@ -8,7 +8,7 @@
 import SwiftUI
 
 struct DetailView: View {
-    @StateObject var viewModel: DetailViewModel
+    @ObservedObject var viewModel: DetailViewModel
     
     var body: some View {
         VStack(spacing: 20) {
